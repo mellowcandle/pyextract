86cf391658138f95597c05cac66e5da8939e406a
Ramon Fried <ramon.fried@gmail.com>
2018-10-24 01:18:52 +0300

    Fix problem with unraring

--- pyextract.py
+++ pyextract.py
@@ -2,14 +2,26 @@
 
 import argparse
 import os
+import subprocess
+import logging
 
 
 def main():
+
+    logging.basicConfig(format='%(message)s')
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("input")
+    parser.add_argument("input", help="File/Folder to extract")
     parser.add_argument("-v", "--verbose", action="store_true", help="Enable Verbosity")
     parser.add_argument("-o", "--output", action='store', help="Output directory")
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    if not os.path.exists(args.input):
+        logging.error("ERROR: Requested file/folder does not exists")
+        exit(-1)
 
     if os.path.isfile(args.input):
         extract_file(args.input, args.output)
@@ -17,14 +29,18 @@ def main():
         extract_dir(args.input)
 
 
-def extract_file(file, output):
-    extract_args = ['7z']
-    extract_args.extend(' x')
+def extract_file(filename, output):
+    extract_cmd = ['7z']
+    extract_cmd.extend(['x', filename])
 
-    if file.endswith('.rar'):
+    if filename.endswith(('.rar','.zip')):
         if output:
-            print(output)
-            extract_args.extend('-o', output)
+            extract_cmd.append('-o' + output)
+
+        subprocess.check_call(extract_cmd)
+
+    else:
+        logging.error("ERROR: Unsupported file format: " + filename)
 
 
 def extract_dir(directory):
